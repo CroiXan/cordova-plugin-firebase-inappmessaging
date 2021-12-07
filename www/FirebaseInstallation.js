5958c6d66da382f377d059c6bfc22d74ed1bcc2c
CroiXan <xan.mark.vi@gmail.com>
2021-12-07 12:57:18 -0300

    Drop Installation logic

--- www/FirebaseInstallation.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-var PLUGIN_NAME = 'FirebaseInstallation';
-
-exports.getInstallationId = function (success, error) {
-  exec(success, error, PLUGIN_NAME, "getInstallationId", []);
-};
-
-
